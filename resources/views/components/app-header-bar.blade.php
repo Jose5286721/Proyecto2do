57e9b4209336054146b444cbca3e79b22467978e
jose-arco <jose-arco@MacBook-Pro-2.local>
2020-11-09 15:17:41 -0300

    cambios en el menu

--- resources/views/components/app-header-bar.blade.php
+++ resources/views/components/app-header-bar.blade.php
@@ -127,98 +127,62 @@
             <nav class="navbar-mobile">
                 <div class="container-fluid">
                     <ul class="navbar-mobile__list list-unstyled">
-                        <li class="has-sub">
-                            <a class="js-arrow" href="#">
+                        <li>
+                            <a href="{{route('home')}}">
                                 <i class="fas fa-tachometer-alt"></i>Dashboard</a>
-                            <ul class="navbar-mobile-sub__list list-unstyled js-sub-list">
-                                <li>
-                                    <a href="index.html">Dashboard 1</a>
-                                </li>
-                                <li>
-                                    <a href="index2.html">Dashboard 2</a>
-                                </li>
-                                <li>
-                                    <a href="index3.html">Dashboard 3</a>
-                                </li>
-                                <li>
-                                    <a href="index4.html">Dashboard 4</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="chart.html">
-                                <i class="fas fa-chart-bar"></i>Charts</a>
-                        </li>
-                        <li>
-                            <a href="table.html">
-                                <i class="fas fa-table"></i>Tables</a>
-                        </li>
-                        <li>
-                            <a href="form.html">
-                                <i class="far fa-check-square"></i>Forms</a>
-                        </li>
-                        <li>
-                            <a href="calendar.html">
-                                <i class="fas fa-calendar-alt"></i>Calendar</a>
-                        </li>
-                        <li>
-                            <a href="map.html">
-                                <i class="fas fa-map-marker-alt"></i>Maps</a>
                         </li>
                         <li class="has-sub">
-                            <a class="js-arrow" href="#">
-                                <i class="fas fa-copy"></i>Pages</a>
-                            <ul class="navbar-mobile-sub__list list-unstyled js-sub-list">
-                                <li>
-                                    <a href="login.html">Login</a>
-                                </li>
-                                <li>
-                                    <a href="register.html">Register</a>
-                                </li>
-                                <li>
-                                    <a href="forget-pass.html">Forget Password</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li class="has-sub">
-                            <a class="js-arrow" href="#">
-                                <i class="fas fa-desktop"></i>UI Elements</a>
-                            <ul class="navbar-mobile-sub__list list-unstyled js-sub-list">
-                                <li>
-                                    <a href="button.html">Button</a>
-                                </li>
-                                <li>
-                                    <a href="badge.html">Badges</a>
-                                </li>
-                                <li>
-                                    <a href="tab.html">Tabs</a>
-                                </li>
-                                <li>
-                                    <a href="card.html">Cards</a>
-                                </li>
-                                <li>
-                                    <a href="alert.html">Alerts</a>
-                                </li>
-                                <li>
-                                    <a href="progress-bar.html">Progress Bars</a>
-                                </li>
-                                <li>
-                                    <a href="modal.html">Modals</a>
-                                </li>
-                                <li>
-                                    <a href="switch.html">Switchs</a>
-                                </li>
-                                <li>
-                                    <a href="grid.html">Grids</a>
-                                </li>
-                                <li>
-                                    <a href="fontawesome.html">Fontawesome Icon</a>
-                                </li>
-                                <li>
-                                    <a href="typo.html">Typography</a>
-                                </li>
-                            </ul>
-                        </li>
+                                <a href="#" class="js-arrow">
+                                    <i class="zmdi zmdi-turning-sign"></i>
+                                    <span class="bot-line"></span>Carreteras</a>
+                                <ul class="navbar-mobile-sub__list list-unstyled js-sub-list">
+                                    <li>
+                                        <a href="{{route('carreteras.index')}}">Listar</a>
+                                    </li>
+                                    <li>
+                                        <a href="{{route('carreteras.create')}}">Crear</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="has-sub">
+                                <a href="#" class="js-arrow">
+                                    <i class="zmdi zmdi-arrow-split"></i>
+                                    <span class="bot-line"></span>Tramos</a>
+                                <ul class="navbar-mobile-sub__list list-unstyled js-sub-list">
+                                    <li>
+                                        <a href="{{route('tramos.index')}}">Listar</a>
+                                    </li>
+                                    <li>
+                                        <a href="{{route('tramos.create')}}">Crear</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="has-sub">
+                                <a href="#" class="js-arrow">
+                                    <i class="zmdi zmdi-accounts"></i>
+                                    <span class="bot-line"></span>Comunidades</a>
+                                <ul class="navbar-mobile-sub__list list-unstyled js-sub-list">
+                                    <li>
+                                        <a href="{{route('comunidades.index')}}">Listar</a>
+                                    </li>
+                                    <li>
+                                        <a href="{{route('comunidades.create')}}">Crear</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="has-sub">
+                                <a href="#" class="js-arrow">
+                                    <i class="zmdi zmdi-accounts-list"></i>
+                                    <span class="bot-line"></span>Tramo Comunidad</a>
+                                <ul class="navbar-mobile-sub__list list-unstyled js-sub-list">
+                                    <li>
+                                        <a href="{{route('tramo_comunidades.index')}}">Listar</a>
+                                    </li>
+                                    <li>
+                                        <a href="{{route('tramo_comunidades.create')}}">Crear</a>
+                                    </li>
+                                </ul>
+                            </li>
                     </ul>
                 </div>
             </nav>
@@ -303,7 +267,7 @@
                 <div class="account-wrap">
                     <div class="account-item account-item--style2 clearfix js-item-menu">
                         <div class="image">
-                            <img src="images/icon/avatar-01.jpg" alt="{{$user->name}}" />
+                            <img src="https://ui-avatars.com/api/?name={{$user->name}}" alt="{{$user->name}}" />
                         </div>
                         <div class="content">
                             <a class="js-acc-btn" href="#">{{$user->name}}</a>
@@ -312,7 +276,7 @@
                             <div class="info clearfix">
                                 <div class="image">
                                     <a href="#">
-                                        <img src="images/icon/avatar-01.jpg" alt="{{$user->name}}" />
+                                        <img src="https://ui-avatars.com/api/?name={{$user->name}}" alt="{{$user->name}}" />
                                     </a>
                                 </div>
                                 <div class="content">
